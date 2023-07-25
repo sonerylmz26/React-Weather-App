fc56f319d354661d4b1f73242ccb654cdb148746
soner_yılmaz <snrylmz3126@gmail.com>
2023-07-25 18:52:22 +0300

    commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h2>Hi React</h2>
+    </div>
+  );
+}
+
+export default App;
